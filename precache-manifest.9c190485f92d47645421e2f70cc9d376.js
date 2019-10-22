f0b6837fdfbd21b016385a4de1e7a7e894f99ea2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-22 09:08:40 +0000

    Deploy Tradeshift/react-tradeshift-ui to github.com/Tradeshift/react-tradeshift-ui.git:gh-pages

---
+++ precache-manifest.9c190485f92d47645421e2f70cc9d376.js
@@ -0,0 +1,23 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb0cf23666e43b2823197036a1912448",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a1318e60514d911c2dfb.bundle.js"
+  },
+  {
+    "revision": "aeb5dc87508ee7034882",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.a1318e60514d911c2dfb.bundle.js"
+  },
+  {
+    "url": "vendors~main.a1318e60514d911c2dfb.bundle.js"
+  },
+  {
+    "revision": "d6f41747d2347dee8272",
+    "url": "vendors~main.css"
+  }
+]);
